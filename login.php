92e954cc953c97145ed8e64249674b2044b16f76
Killiam <killiam1119@gmail.com>
2025-06-25 14:22:33 -0500

    Hora local Bogotá en notificaciones

--- login.php
+++ login.php
@@ -1,4 +1,5 @@
 <?php
+date_default_timezone_set("America/Bogota");
 if (session_status() === PHP_SESSION_NONE) {
    session_start();
 }
--- logout.php
+++ logout.php
@@ -1,4 +1,5 @@
 <?php
+date_default_timezone_set("America/Bogota");
 // Iniciar la sesión
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
